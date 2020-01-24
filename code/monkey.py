90c1e4a78cba40f89abb9ec3ce27a4e347ce5e5e
TylersDurden <srobbin1@ithaca.edu>
2020-01-23 19:37:25 -0500

    test

--- code/monkey.py
+++ code/monkey.py
@@ -12,7 +12,8 @@ def honey(port, run_time, verbose, reply):
     date, local_time = utils.create_timestamp()
     banner = 'Honey pot created [port %d] - %s %s\n' % (port, local_time, date)
     if verbose:
-        print banner
+        print 'Honey pot created [port %d] - %s %s' % (port, local_time, date)
+        print '[*] Initialized Uptime: %f hours' % (run_time/3600.)
     data = ''
     log_file = date.replace('/','')+'_'+local_time.split(':')[0]+local_time.split(':')[1]+'.log'
     open(log_file, 'wb').write(banner)
@@ -27,6 +28,8 @@ def honey(port, run_time, verbose, reply):
                 clients.append(client_adr[0])
             except socket.error:
                 print '[*] Connection Broken with %s' % client_adr[0]
+            except:
+                pass
 
             msg = '[*] Connection accepted-> %s:%d' % (client_adr[0], client_adr[1])
             data += '[*] Connection accepted from %s at %s - %s\n' % (client_adr[0], date, local_time)
@@ -54,7 +57,7 @@ if 'basic' in sys.argv:
     print '[!] Server Killed [%ss Elapsed]' % str(time.time()-start)
 
 if 'http' in sys.argv:
-    honey(80, 60, True, open('templates/index.html').read())
+    honey(80, 1e5, True, open('templates/index.html').read())
 
 if 'ssh' in sys.argv:
     honey(22, 60, True, '\033[31m')
