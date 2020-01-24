eb32698425f7473bebe1ebf47f6d0fbde79f0ac2
TylersDurden <srobbin1@ithaca.edu>
2020-01-23 19:27:09 -0500

    test

--- code/honey.py
+++ code/honey.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
@@ -15,44 +15,11 @@ def swap(fname, destroy):
         os.system('rm '+fname)
     return data
 
-# def get_page(dir, file):
-#     filename = (file)
-#
-#     if filename in cache:
-#         return cache[filename]
-#
-#     path = os.path.abspath(os.path.join(os.path.dirname(__file__), dir,filename))
-#     try:
-#         file_contents = open(path, encoding='utf-8').read()
-#     except:
-#         return None
-#
-#     data, text = file_contents.split('---\n', 1)
-#     page = yaml.load(data)
-#     page['content'] = Markup(markdown.markdown(text))
-#     page['path'] = file
-#
-#     cache[filename] = page
-#     return page
-# @app.route('/')
-# def login():
-#     return render_template('sample.html')
-#
-#
-# @app.route('/login')
-# def flask_captive():
-#     return render_template('login.html')
-
 
 @app.route('/')
 def hello_friend():
-    cmd = "curl https://ipinfo.io/$(curl 'https://api.ipify.org?format=json' | jq -r .'ip')"
-    os.system(cmd + ' >> user.txt')
-    #os.system('echo $(' + cmd + ")")
-    data = ''
-    for line in swap('user.txt', True):
-        data += line
-    print 'CLICKED BY ' + data
+    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    print client_ip
     return render_template('index.html')
 
 ###########################################################################################
--- code/monkey.py
+++ code/monkey.py
@@ -4,13 +4,16 @@ import time
 import sys
 import os
 
+log_timer = 30  # seconds between log updates
+
 
 def honey(port, run_time, verbose, reply):
+    clients = []
     date, local_time = utils.create_timestamp()
     banner = 'Honey pot created [port %d] - %s %s\n' % (port, local_time, date)
     if verbose:
         print banner
-
+    data = ''
     log_file = date.replace('/','')+'_'+local_time.split(':')[0]+local_time.split(':')[1]+'.log'
     open(log_file, 'wb').write(banner)
     s = utils.start_listener(port, 5)
@@ -21,12 +24,20 @@ def honey(port, run_time, verbose, reply):
             try:
                 client.send(reply)
                 client.close()
+                clients.append(client_adr[0])
             except socket.error:
                 print '[*] Connection Broken with %s' % client_adr[0]
-            msg = '[*] Connection accepted-> %s:%d\n' % (client_adr[0], client_adr[1])
+
+            msg = '[*] Connection accepted-> %s:%d' % (client_adr[0], client_adr[1])
+            data += '[*] Connection accepted from %s at %s - %s\n' % (client_adr[0], date, local_time)
             if verbose:
                 print msg
-            open(log_file, 'a').write(msg)
+            if int(time.time()-tic)%1==0:
+                date, local_time = utils.create_timestamp()
+            if int(time.time() - tic) % log_timer == 0:
+                print '[*] Logging Connections [%ss Elapsed]' % str(time.time()-tic)
+                open(log_file, 'wb').write(data)
+                data = ''
 
     except KeyboardInterrupt:
         print '[!!] Server Killed [%ss Elapsed]' % str(time.time() - tic)
@@ -43,7 +54,7 @@ if 'basic' in sys.argv:
     print '[!] Server Killed [%ss Elapsed]' % str(time.time()-start)
 
 if 'http' in sys.argv:
-    honey(80, 60, True, open('index.html').read())
+    honey(80, 60, True, open('templates/index.html').read())
 
 if 'ssh' in sys.argv:
     honey(22, 60, True, '\033[31m')
